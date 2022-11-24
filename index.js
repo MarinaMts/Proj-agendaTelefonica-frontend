74ab014719eb5fbcf86441458929dcaf9e785d52
MarinaMts <marina.martinss_@outlook.com>
2022-11-23 21:58:45 -0300

    CRIAÇÃO FRONT END

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const app = express();
+const axios = require('axios').default;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/cadastroCategorias', (req, res)=>{
+    res.render('categoria/index');
+});
+
+app.get('/listagemCategorias', (req, res)=>{
+    
+    const urlListagemCategoria = 'http://localhost:3000/listarCategoria';
+    axios.get(urlListagemCategoria)
+        .then(
+            (response)=>{
+                let categorias = response.data;
+                res.render('categoria/listagemCategoria', {categorias});
+        }); 
+    });
+
+    app.get('/formEdicaoCategorias/:id', (req, res)=>{
+        
+        let {id} = req.params;
+        const urlListagemCategoria = `http://localhost:3000/listarCategoria/${id}`;
+        axios.get(urlListagemCategoria)
+        .then(
+            (response)=>{
+                let categoria = response.data;
+                res.render('categoria/editarCategoria', {categoria});
+            }
+        )
+    });
+
+    app.post('/alterarCategoria', (req, res)=>{
+        const urlAlterarCategoria = 'http://localhost:3000/alterarCategoria';
+        console.log(req.body);
+        axios.put(urlAlterarCategoria, req.body)
+        .then(
+            res.send('ALTERADO!')
+        )
+    });
+
+app.listen(3001, ()=>{
+    console.log('SERVIDOR RODANDO EM: http://localhost:3001');
+});
